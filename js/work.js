c829afbf76af25b5ae2b26e1a7957f6e0b9cd8bb
Alexander Golin <agolin95@gmail.com>
2021-06-22 01:02:05 -0400

    nicer work grid, add resume, try iframe viewer for pdf

--- js/work.js
+++ js/work.js
@@ -12,11 +12,12 @@ $(function() {
         makeTrackElems(json);
         $(".workItem").click(function() {
             let file = $(this).data("file");
-            let fullpath = "/work/" + file;
+            let fullpath = "https://alexandergolin.com/work/" + file;
             let type = file.split(".")[1];
             let html = "";
             if (type == "pdf") {
-                html = `<embed src="${fullpath}"/>`;
+                html = `<iframe src="http://docs.google.com/gview?url=${fullpath}&embedded=true" frameborder="0"></iframe>`
+                // html = `<embed src="${fullpath}"/>`;
             } else if (type == "png" || type == "jpg") {
                 html = `<img src="${fullpath}"/>`
             }
@@ -36,7 +37,7 @@ $(function() {
 function makeTrackElems(json) {
     for (let i in json) {
         let html = 
-            `<div class="workItem" data-file="${json[i].file}">
+            `<div class="workItem" data-file="${json[i].file}" style="background-image: url(/work/${json[i].file.split('.')[0]}.png)">
                 <h3 class="workTitle">${json[i].title}</h3>
                 <h4 class="workDate">${json[i].date}</h4>
                 <p class="workSummary">${json[i].summary}</p>
